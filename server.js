fa1a868bc4fe6ba396cde26e669a9f942ca182f1
Dharini Ramakrishnan <dharini.ramakrishnan@gmail.com>
2016-01-18 16:18:09 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('Private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log(req.method+' '+ req.originalUrl);
+		next();
+	}
+}
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+})
+
+app.use(express.static(__dirname+'/public'));
+console.log(__dirname);
+app.listen(port, function() {
+	console.log('Express server started on port '+port);
+});
